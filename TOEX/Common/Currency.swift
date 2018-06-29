a12d44b798ac51207e87e4de5227fbdd7ad694ed
TaeGeun Moon <ahn_v3@naver.com>
2018-06-30 00:40:05 +0900

    currency model added

---
+++ TOEX/Common/Currency.swift
@@ -0,0 +1,41 @@
+//
+//  Currency.swift
+//  TOEX
+//
+//  Created by TaeGeun Moon on 2018. 6. 30..
+//  Copyright © 2018년 TaeGeun Moon. All rights reserved.
+//
+
+import Foundation
+
+class Currency{
+    enum Unit: String{
+        case USD, KRW, JPY, EUR
+        
+        var flagImage: UIImage{
+            switch(self){
+            case .EUR:
+                return #imageLiteral(resourceName: "flag_england")
+            case .KRW:
+                return #imageLiteral(resourceName: "flag_kor")
+            case .JPY:
+                return #imageLiteral(resourceName: "flag_japan")
+            case .USD:
+                return #imageLiteral(resourceName: "flag_usa")
+            }
+        }
+        
+        var character: Character{
+            switch(self){
+            case .EUR:
+                return "€"
+            case .KRW:
+                return "₩"
+            case .JPY:
+                return "¥"
+            case .USD:
+                return "$"
+            }
+        }
+    }
+}
